26a02bf1d1c5b6c1a4a8c7c670ca291b74bf5753
mecolmg <mecolmg@vt.edu>
2018-11-02 16:11:18 -0400

    Completed Project Checkpoint #1.

--- noahGoesOutside.js
+++ noahGoesOutside.js
@@ -1,26 +1,82 @@
 function sketchProc(processing) {
   with (processing) {
     var setup = function() {
-      size(700, 400);
+      size(840, 640);
       frameRate(60);
     };
 
     // CONSTANTS:
-    var SCALE = 0.8;
+    var DEBUG_MODE = false;
+    var SCALE = 2;
     var CAMERA_BUFFER = 2;
-    var JUMP_VELOCITY = 16;
-    var GRAVITY = new PVector(0, 1);
-    var DEFAULT_MAXDX = 6;
-    var DEFAULT_MAXDY = 20;
+    var GRAVITY = new PVector(0, 0.5, 0);
+    var PLAYER_JUMPS = 2;
+    var DEFAULT_MAXDX = 3;
+    var DEFAULT_MAXDY = 10;
     var DEFAULT_FRICTION = 0.7;
     var DEFAULT_AIR_RES = 0.98;
+    var DEFAULT_JUMP_VELOCITY = 8;
+    var DEFAULT_FPS = 7;
 
     // GLOBALS:
+    var gameInit = false;
     var keys = [];
+    var spritesheets = {};
+    var REQUESTED_IMAGES = [];
 
     // uc = undefined check. This is a quick
     var uc = function(x) {
       return x !== undefined;
+    };
+
+    var getImage = function(url) {
+      var image = requestImage(url);
+      REQUESTED_IMAGES.push(image);
+      return image;
+    };
+
+    var checkRequestedImages = function(callback) {
+      for (var i = 0; i < REQUESTED_IMAGES.length; i++) {
+        if (REQUESTED_IMAGES[i].loaded) {
+          REQUESTED_IMAGES.splice(i, 1);
+        } else {
+          setTimeout(checkRequestedImages, 100, callback);
+          return false;
+        }
+      }
+      if (callback) {
+        callback();
+      }
+      return true;
+    };
+
+    var fetchSpritesheets = function(tilemap) {
+      for (var j = 0; j < tilemap.tilesets.length; j++) {
+        var tileset = tilemap.tilesets[j];
+        if (tileset.image) {
+          spritesheets[tileset.image] = getImage(tileset.image);
+        }
+      }
+    };
+
+    var getTileMap = function(tilemap) {
+      tilemap.layers.forEach(function(layer) {
+        var props = {};
+        (layer.properties || []).forEach(function(prop) {
+          props[prop.name] = prop.value;
+        });
+        layer.props = props;
+      });
+      tilemap.tilesets.forEach(function(tileset) {
+        (tileset.tiles || []).forEach(function(tile) {
+          var props = {};
+          (tile.properties || []).forEach(function(prop) {
+            props[prop.name] = prop.value;
+          });
+          tile.props = props;
+        });
+      });
+      return tilemap;
     };
 
     var getTiles = function(tilemap) {
@@ -40,20 +96,39 @@ function sketchProc(processing) {
             id >= tileset.firstgid &&
             id < tileset.firstgid + tileset.tilecount
           ) {
-            var tile = tileset.tiles[id - tileset.firstgid];
-            var props = {};
-            if (tile.properties) {
-              tile.properties.forEach(function(prop) {
-                props[prop.name] = prop.value;
+            if (tileset.image) {
+              var tileIdx = id - tileset.firstgid;
+              var r = floor(tileIdx / tileset.columns);
+              var c = tileIdx - r * tileset.columns;
+              var x1 =
+                tileset.margin + (tileset.tilewidth + tileset.spacing) * c;
+              var y1 =
+                tileset.margin + (tileset.tileheight + tileset.spacing) * r;
+              var image = spritesheets[tileset.image].get(
+                x1,
+                y1,
+                tileset.tilewidth,
+                tileset.tileheight
+              );
+              var tile = (tileset.tiles || []).find(function(tile) {
+                return tile.id === tileIdx;
               });
+              tiles[id] = {
+                image,
+                width: tileset.tilewidth,
+                height: tileset.tileheight,
+                props: tile ? tile.props : {}
+              };
+              return;
+            } else if (tileset.tiles) {
+              tiles[id] = {
+                image: loadImage(tile.image),
+                width: tile.imagewidth,
+                height: tile.imageheight,
+                props: tile.props || {}
+              };
+              return;
             }
-            tiles[id] = {
-              image: loadImage(tile.image),
-              width: tile.imagewidth,
-              height: tile.imageheight,
-              props
-            };
-            return;
           }
         }
         console.error("ID " + id + " was not loaded.");
@@ -61,10 +136,24 @@ function sketchProc(processing) {
       return tiles;
     };
 
+    var getPoint = function(tilemap, pointName) {
+      for (var i = 0; i < tilemap.layers.length; i++) {
+        if (tilemap.layers[i].type === "objectgroup") {
+          var layer = tilemap.layers[i];
+          var point = layer.objects.find(function(x) {
+            return x.name === pointName;
+          });
+          if (point) {
+            return point;
+          }
+        }
+      }
+    };
+
     class Positionable {
       constructor(x, y, _width, _height, angle) {
-        this.p = new PVector(x, y);
-        this.v = new PVector(0, 0);
+        this.p = new PVector(x, y, 0);
+        this.v = new PVector(0, 0, 0);
         this.w = _width;
         this.h = _height;
         this.a = angle;
@@ -72,6 +161,7 @@ function sketchProc(processing) {
         this.maxdy = DEFAULT_MAXDY;
         this.friction = DEFAULT_FRICTION;
         this.airRes = DEFAULT_AIR_RES;
+        this.jumpVelocity = -DEFAULT_JUMP_VELOCITY;
         this.falling = false;
         this.cpOffsets = {
           tl: { x: 0, y: -this.h },
@@ -89,13 +179,18 @@ function sketchProc(processing) {
       addVelocity(vec) {
         this.v.set(
           max(min(this.v.x + vec.x, this.maxdx), -this.maxdx),
-          max(min(this.v.y + vec.y, this.maxdy), -this.maxdy)
+          max(min(this.v.y + vec.y, this.maxdy), -this.maxdy),
+          0
         );
       }
 
       updatePosition() {
         this.p.add(this.v);
         this.v.mult(this.falling ? this.airRes : this.friction);
+      }
+
+      moving() {
+        return this.v.mag() > 0.1;
       }
 
       collisionPoints() {
@@ -107,6 +202,103 @@ function sketchProc(processing) {
           };
         }, this);
         return cps;
+      }
+
+      getCollisions(tilemap, tiles) {
+        var cps = this.collisionPoints();
+        var collisions = {};
+        Object.keys(cps).forEach(function(k) {
+          var tx = floor(cps[k].x / tilemap.tilewidth);
+          var ty = floor(cps[k].y / tilemap.tileheight);
+          if (tx < 0 || tx >= tilemap.width) {
+            collisions[k] = { x: tx, y: ty, collideTop: false, collide: true };
+            return;
+          }
+          var tile = null;
+          var tileLayer = null;
+          tilemap.layers.find(function(layer) {
+            if (layer.type !== "tilelayer" || layer.props.background) {
+              return false;
+            }
+            var id = layer.data[ty * layer.height + tx];
+            if (id !== 0) {
+              tile = tiles[id];
+              tileLayer = layer;
+              return true;
+            }
+            return false;
+          });
+          collisions[k] = { x: tx, y: ty, collideTop: false, collide: false };
+          if (tile) {
+            collisions[k] = {
+              ...collisions[k],
+              collideTop:
+                tileLayer.props.collideTop ||
+                tileLayer.props.collide ||
+                tile.props.collideTop ||
+                tile.props.collide,
+              collide: tileLayer.props.collide || tile.props.collide
+            };
+          }
+        }, this);
+        return collisions;
+      }
+
+      handleCollision(tilemap, tiles) {
+        var fallingDown = this.falling && this.v.y > 0;
+        var jumpingUp = this.falling && this.v.y < 0;
+        var movingLeft = this.v.x < 0;
+        var movingRight = this.v.x > 0;
+        var c = this.getCollisions(tilemap, tiles);
+        if (movingLeft && (c.ml.collide || c.tl.collide)) {
+          this.collideLeft();
+          return true;
+        }
+        if (movingRight && (c.mr.collide || c.tr.collide)) {
+          this.collideRight();
+          return true;
+        }
+        if (jumpingUp && !c.ml.collide && (c.tl.collide || c.tr.collide)) {
+          this.collideTop();
+          return true;
+        }
+        if (fallingDown && (c.bl.collideTop || c.br.collideTop)) {
+          this.collideBottom();
+          return true;
+        }
+        if (!this.falling && !c.bl.collideTop && !c.br.collideTop) {
+          return this.startFalling();
+        }
+        return false;
+      }
+
+      collideLeft() {
+        this.p.x = ceil(this.p.x / tilemap.tilewidth) * tilemap.tilewidth;
+        this.v.x = 0;
+      }
+
+      collideRight() {
+        this.p.x =
+          ceil(this.p.x / tilemap.tilewidth) * tilemap.tilewidth - this.w - 1;
+        this.v.x = 0;
+      }
+
+      collideTop() {
+        this.p.y =
+          floor(this.p.y / tilemap.tileheight) * tilemap.tileheight +
+          this.h +
+          1;
+        this.v.y = 0;
+      }
+
+      collideBottom() {
+        this.falling = false;
+        this.p.y = floor(this.p.y / tilemap.tileheight) * tilemap.tileheight;
+        this.v.y = 0;
+      }
+
+      startFalling() {
+        this.falling = true;
       }
 
       draw() {
@@ -121,20 +313,86 @@ function sketchProc(processing) {
       }
     }
 
-    class Boundary extends Positionable {
-      constructor(x, y, _width, _height, angle) {
-        super(x, y, _width, _height, angle);
+    class Character extends Positionable {
+      constructor(x, y, w, h, walkImgs, jumpImgs) {
+        super(x, y, w, h, 0);
+        this.walkImgs = walkImgs;
+        this.jumpImgs = jumpImgs;
+        this.fps = DEFAULT_FPS;
+        this.animationFrame = 0;
+        this.img = this.walkImgs[this.animationFrame];
+        this.animation = "walk";
+        this.dir = LEFT;
+      }
+
+      jump() {
+        this.v.y = this.jumpVelocity;
+        this.falling = true;
+      }
+
+      moveLeft() {
+        this.addVelocity(new PVector(-0.8, 0, 0));
+        this.dir = LEFT;
+      }
+
+      moveRight() {
+        this.addVelocity(new PVector(0.8, 0, 0));
+        this.dir = RIGHT;
+      }
+
+      stepAnimation() {
+        if (this.falling) {
+          if (this.animation === "walk") {
+            this.animation = "jump";
+            this.animationFrame = 0;
+            this.img = this.jumpImgs[this.animationFrame];
+          } else {
+            if (frameCount % floor(60 / this.fps) === 0) {
+              this.animationFrame =
+                (this.animationFrame + 1) % this.jumpImgs.length;
+              this.img = this.jumpImgs[this.animationFrame];
+            }
+          }
+        } else if (!this.moving()) {
+          this.animation = "walk";
+          this.animationFrame = 0;
+          this.img = this.walkImgs[this.animationFrame];
+        } else {
+          if (this.animation !== "walk") {
+            this.animation = "walk";
+            this.animationFrame = 0;
+            this.img = this.walkImgs[this.animationFrame];
+          } else {
+            if (frameCount % floor(60 / this.fps) === 0) {
+              this.animationFrame =
+                (this.animationFrame + 1) % this.walkImgs.length;
+              this.img = this.walkImgs[this.animationFrame];
+            }
+          }
+        }
+      }
+
+      draw() {
+        if (DEBUG_MODE) {
+          super.draw();
+        }
+        this.stepAnimation();
+        pushMatrix();
+        imageMode(CENTER);
+        translate(this.p.x + this.w / 2, this.p.y - this.h / 2);
+        if (this.dir === LEFT) {
+          scale(-1, 1);
+        }
+        image(this.img, 0, 0);
+        imageMode(CORNER);
+        popMatrix();
       }
     }
 
-    class Player extends Positionable {
-      constructor(x, y, w, h, img) {
-        super(x, y, w, h, 0);
-        this.v = new PVector(0, 0);
-        this.w = w; // Width
-        this.h = h; // Height
-        this.img = img; // Image
-        this.jumps = 2;
+    class Player extends Character {
+      constructor(x, y, w, h, walkImgs, jumpImgs) {
+        super(x, y, w, h, walkImgs, jumpImgs);
+        this.jumps = PLAYER_JUMPS;
       }
 
       update(tilemap, tiles) {
@@ -142,99 +400,41 @@ function sketchProc(processing) {
           this.addVelocity(GRAVITY);
         }
         this.updatePosition();
-        while (this.checkCollision(tilemap, tiles));
+        while (this.handleCollision(tilemap, tiles));
+        this.handleInput();
+      }
+
+      handleInput() {
         if (keys[LEFT]) {
-          this.addVelocity(new PVector(-2, 0));
+          this.moveLeft();
         } else if (keys[RIGHT]) {
-          this.addVelocity(new PVector(2, 0));
+          this.moveRight();
         }
         if (keys[UP] && (!this.falling || this.jumps > 0)) {
           keys[UP] = false;
           this.jumps--;
-          this.v.y = -JUMP_VELOCITY;
-          this.falling = true;
+          this.jump();
         }
       }
 
-      checkCollision(tilemap, tiles) {
-        var fallingDown = this.falling && this.v.y > 0;
-        var jumpingUp = this.falling && this.v.y < 0;
-        var movingLeft = this.v.x < 0;
-        var movingRight = this.v.x > 0;
-        var cps = this.collisionPoints();
-        var c = {};
-        Object.keys(cps).forEach(function(k) {
-          var tx = floor(cps[k].x / tilemap.tilewidth);
-          var ty = floor(cps[k].y / tilemap.tileheight);
-          var tile = null;
-          tilemap.layers.find(function(layer) {
-            if (layer.type !== "tilelayer") {
-              return false;
-            }
-            var id = layer.data[ty * layer.height + tx];
-            if (id !== 0) {
-              tile = tiles[id];
-              return true;
-            }
-            return false;
-          });
-          c[k] = { x: tx, y: ty, collideTop: false, collide: false };
-          if (tile) {
-            c[k] = {
-              ...c[k],
-              collideTop: tile.props.collideTop || tile.props.collide,
-              collide: tile.props.collide
-            };
-          }
-        }, this);
-        if (jumpingUp && c.ml.collide && (c.tl.collide || c.tr.collide)) {
-          this.p.y =
-            floor(this.p.y / tilemap.tileheight) * tilemap.tileheight +
-            this.h +
-            1;
-          this.v.y = 0;
+      handleCollision(tilemap, tiles) {
+        if (super.handleCollision(tilemap, tiles)) {
           return true;
-        }
-        if (movingLeft && (c.ml.collide || c.tl.collide)) {
-          this.p.x = ceil(this.p.x / tilemap.tilewidth) * tilemap.tilewidth + 1;
-          this.v.x = 0;
-          return true;
-        }
-        if (movingRight && (c.mr.collide || c.tr.collide)) {
-          this.p.x =
-            ceil(this.p.x / tilemap.tilewidth) * tilemap.tilewidth - this.w - 1;
-          this.v.x = 0;
-          return true;
-        }
-        if (jumpingUp && !c.ml.collide && (c.tl.collide || c.tr.collide)) {
-          this.p.y =
-            floor(this.p.y / tilemap.tileheight) * tilemap.tileheight +
-            this.h +
-            1;
-          this.v.y = 0;
-          return true;
-        }
-        if (
-          fallingDown &&
-          (c.bl.collide ||
-            c.br.collide ||
-            ((c.bl.collideTop || c.br.collideTop) &&
-              !(c.ml.collideTop || c.mr.collideTop)))
-        ) {
-          this.falling = false;
-          this.p.y = floor(this.p.y / tilemap.tileheight) * tilemap.tileheight;
-          this.v.y = 0;
-          this.jumps = 2;
-          return true;
-        }
-        if (!this.falling && !c.bl.collideTop && !c.br.collideTop) {
-          this.falling = true;
         }
         return false;
       }
+
+      collideBottom() {
+        super.collideBottom();
+        this.resetJumps();
+      }
+
+      resetJumps() {
+        this.jumps = PLAYER_JUMPS;
+      }
     }
 
-    var Camera = function(_scale, buffer) {
+    var Camera = function(scale_, buffer) {
       this.x = 0;
       this.y = 0;
       this.cx = 0;
@@ -243,7 +443,7 @@ function sketchProc(processing) {
       this.ry = 0;
       this.r = 0;
       this.c = 0;
-      this.scale = _scale;
+      this.scale = scale_;
       this.buffer = buffer;
       this.update = function(player, map) {
         var mapW = map.width * map.tilewidth,
@@ -286,14 +486,55 @@ function sketchProc(processing) {
       }
     };
 
-    var tilemap = TileMaps.TileMap1;
-    var tiles = getTiles(tilemap);
-    var player1 = new Player(105, 35, 64, 64, null);
-    var camera = new Camera(SCALE, CAMERA_BUFFER);
+    var NOAH_WALK_IMGS = [
+      getImage("NGO_Noah/noah_walk_0.png"),
+      getImage("NGO_Noah/noah_walk_1.png"),
+      getImage("NGO_Noah/noah_walk_2.png"),
+      getImage("NGO_Noah/noah_walk_3.png")
+    ];
+    var NOAH_JUMP_IMGS = [getImage("NGO_Noah/noah_walk_1.png")];
+    var NGO_TITLE = getImage("NGO_title.png");
+    var NGO_INSTRUCTIONS = getImage("NGO_instructions.png");
+    var NGO_INSTRUCTIONS_SCREEN = getImage("NGO_instructions_screen.png");
+    var NGO_CREATOR = getImage("NGO_creator.png");
+    var NGO_COMING_SOON = getImage("NGO_coming_soon.png");
+
+    var tiles, noah, camera;
+    var tilemap = getTileMap(TileMaps.HomeScreenMap);
+    var init = function() {
+      tiles = getTiles(tilemap);
+      var start = getPoint(tilemap, "start") || { x: 100, y: 100 };
+      noah = new Player(
+        start.x,
+        start.y,
+        18,
+        18,
+        NOAH_WALK_IMGS,
+        NOAH_JUMP_IMGS
+      );
+      camera = new Camera(SCALE, CAMERA_BUFFER);
+      gameInit = true;
+    };
+    fetchSpritesheets(tilemap);
+    checkRequestedImages(init);
+
+    var showInstructions = false;
+    var level = "start";
+    var exit = getPoint(tilemap, "exit") || { x: 0, y: height };
 
     var keyPressed = function() {
       keys[keyCode] = true;
       keys[key] = true;
+      if (keys["?"]) {
+        showInstructions = !showInstructions;
+      }
+      if (keys[" "]) {
+        var exitLoc = new PVector(exit.x, exit.y, 0);
+        console.log(exitLoc, noah.p.dist(exitLoc));
+        if (noah.p.dist(exitLoc) < tilemap.tilewidth) {
+          level = "blank";
+        }
+      }
     };
 
     var keyReleased = function() {
@@ -302,17 +543,43 @@ function sketchProc(processing) {
     };
 
     var draw = function() {
-      player1.update(tilemap, tiles);
-      camera.update(player1, tilemap);
+      if (!gameInit) return;
+      noah.update(tilemap, tiles);
+      camera.update(noah, tilemap);
 
       pushMatrix();
-      background(152, 229, 236);
-      camera.show();
-      tilemap.layers.forEach(function(layer) {
-        drawLayer(camera, layer, tilemap, tiles);
-      });
-      player1.draw();
+      background(0, 0, 0);
+      switch (level) {
+        case "start": {
+          camera.show();
+          tilemap.layers.forEach(function(layer) {
+            drawLayer(camera, layer, tilemap, tiles);
+          });
+          noah.draw();
+          break;
+        }
+        case "blank": {
+          image(
+            NGO_COMING_SOON,
+            width / 2 - NGO_COMING_SOON.width / 2,
+            height / 2 - NGO_COMING_SOON.height / 2
+          );
+          break;
+        }
+      }
       popMatrix();
+      if (level === "start") {
+        image(NGO_TITLE, width / 2 - NGO_TITLE.width / 2, 10);
+        image(NGO_INSTRUCTIONS, width / 2 - NGO_INSTRUCTIONS.width / 2, 280);
+        image(NGO_CREATOR, width / 2 - NGO_CREATOR.width / 2, 330);
+      }
+      if (showInstructions) {
+        fill(20, 20, 20, 220);
+        noStroke();
+        rect(20, 40, width - 40, height - 80);
+        rect(40, 20, width - 80, height - 40);
+        image(NGO_INSTRUCTIONS_SCREEN, 40, 40);
+      }
     };
   }
 }
